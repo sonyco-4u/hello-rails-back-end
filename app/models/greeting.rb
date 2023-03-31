2703667f4ea8c8efe30f7f1d7c57b732f3b95124
Ezema Anthony Sunday <tonyunday@gmail.com>
2023-03-31 09:43:46 -0700

    Added comment update

--- app/models/greeting.rb
+++ app/models/greeting.rb
@@ -1,3 +1,4 @@
+# Define a database column named 'message' with a 'text' data type
 class Greeting < ApplicationRecord
   attribute :message, :text
 end
--- db/migrate/20230330142325_create_greetings.rb
+++ db/migrate/20230330142325_create_greetings.rb
@@ -1,3 +1,5 @@
+# Add a 'message' column with a 'text' data type instead of 'text' column
+ # Add 'created_at' and 'updated_at' timestamp columns
 class CreateGreetings < ActiveRecord::Migration[7.0]
   def change
     create_table :greetings do |t|
